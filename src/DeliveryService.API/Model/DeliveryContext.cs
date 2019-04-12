50c8d38ff9be6f0dc5b5091271055f081ba824d7
Renan Bezerra <renan.bezerra@ancud.de>
2019-04-12 02:51:08 +0200

    Adding data by migrations

--- src/DeliveryService.API/Model/DeliveryContext.cs
+++ src/DeliveryService.API/Model/DeliveryContext.cs
@@ -15,6 +15,7 @@ namespace DeliveryService.API.Model
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
             modelBuilder.Entity<Point>()
                 .HasMany(p => p.Origins)
                 .WithOne(r => r.Origin)
@@ -26,6 +27,121 @@ namespace DeliveryService.API.Model
                 .WithOne(r => r.Destination)
                 .HasForeignKey(p => p.DestinationId)
                 .IsRequired(false);
+
+
+            modelBuilder.Entity<Point>().HasData(
+                new
+                {
+                    Id = 1,
+                    Name = "A"
+                },
+                new
+                {
+                    Id = 2,
+                    Name = "B"
+                },
+                new
+                {
+                    Id = 3,
+                    Name = "C"
+                },
+                new
+                {
+                    Id = 4,
+                    Name = "D"
+                },
+                new
+                {
+                    Id = 5,
+                    Name = "E"
+                },
+                new
+                {
+                    Id = 7,
+                    Name = "K"
+                });
+
+            modelBuilder.Entity<PointsConnection>().HasData(
+                new
+                {
+                    Id = 3,
+                    OriginId = 1,
+                    DestinationId = 2,
+                    Time = 1,
+                    Cost = 5
+                },
+                new
+                {
+                    Id = 5,
+                    OriginId = 2,
+                    DestinationId = 3,
+                    Time = 2,
+                    Cost = 10
+
+                },
+                new
+                {
+                    Id = 6,
+                    OriginId = 1,
+                    DestinationId = 3,
+                    Time = 2,
+                    Cost = 2
+
+                },
+                new
+                {
+                    Id = 7,
+                    OriginId = 1,
+                    DestinationId = 4,
+                    Time = 4,
+                    Cost = 20
+
+                },
+                new
+                {
+                    Id = 8,
+                    OriginId = 4,
+                    DestinationId = 5,
+                    Time = 2,
+                    Cost = 10
+
+                },
+                new
+                {
+                    Id = 9,
+                    OriginId = 5,
+                    DestinationId = 3,
+                    Time = 3,
+                    Cost = 15
+
+                },
+                new
+                {
+                    Id = 10,
+                    OriginId = 4,
+                    DestinationId = 7,
+                    Time = 3,
+                    Cost = 15
+
+                },
+                new
+                {
+                    Id = 11,
+                    OriginId = 7,
+                    DestinationId = 5,
+                    Time = 2,
+                    Cost = 10
+
+                },
+                new
+                {
+                    Id = 12,
+                    OriginId = 1,
+                    DestinationId = 7,
+                    Time = 5,
+                    Cost = 25
+
+                });
         }
     }
 }
